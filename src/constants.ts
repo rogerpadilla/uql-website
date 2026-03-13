770bdcde0cd925f7ede8241846dba28bda081a72
Roger Padilla <sonemonu@gmail.com>
2026-03-13 13:45:43 -0500

    refactor: Remove Discord social links and update Biome dependency to 2.4.7.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
 export const projectDescription = 'The smartest ORM for TypeScript and modern JavaScript designed to be fast, safe, and easy to integrate into any application.';
 export const githubRepo = 'https://github.com/rogerpadilla/uql';
-export const discordInvite = 'https://discord.gg/DHJYp6MDS7';
