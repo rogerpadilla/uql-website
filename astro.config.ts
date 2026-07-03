2b6c2321d699b7a2b6d48404b38c30e30c47c2c3
Roger Padilla <rpadilla@auditboard.com>
2026-07-03 10:09:01 -0500

    docs(nav): rename Querying group to Queries, parallel to Entities

--- astro.config.ts
+++ astro.config.ts
@@ -118,7 +118,7 @@ const config: AstroUserConfig = {
           ],
         },
         {
-          label: 'Querying',
+          label: 'Queries',
           items: [{ autogenerate: { directory: 'querying' } }],
         },
         {
